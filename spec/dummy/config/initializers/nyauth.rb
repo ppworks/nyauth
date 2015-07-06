2d03e92edaafd0ca99d0484f0558a2c6fb20cd46
koshikawa <koshikawa@ppworks.jp>
2015-07-07 00:30:33 +0900

    Initializer for dummy app

---
+++ spec/dummy/config/initializers/nyauth.rb
@@ -0,0 +1,3 @@
+Nyauth.configure do |config|
+  config.encryption_secret = ENV['NYAUTH_ENCRYPTION_SECRET'] || 'rails6dd08eff1c2cf5aecb54cb1a97266817b58cc27f06be9e95918c06607d3950d623a4fd4c0c306d2216cdaf3f99871e21e0e975a5e64ef5cf286b68ed8d7379a4'
+end
