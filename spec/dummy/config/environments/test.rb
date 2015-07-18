57a3f6592028244d36a902ed2d6b0acbe5066804
koshikawa <koshikawa@ppworks.jp>
2015-07-19 01:55:26 +0900

    Use memory store cache

--- spec/dummy/config/environments/test.rb
+++ spec/dummy/config/environments/test.rb
@@ -41,4 +41,5 @@ Rails.application.configure do
   # config.action_view.raise_on_missing_translations = true
 
   config.action_mailer.default_url_options = { host: 'www.example.com' }
+  config.cache_store = :memory_store
 end
