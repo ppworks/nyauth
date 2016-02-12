94d24edb380f1587ffb1e2331c949324954ab7d5
ppworks <koshikawa@ppworks.jp>
2016-02-12 17:40:26 +0900

    Make helper method on controller private

--- spec/support/controllers/nyauth/session_concern.rb
+++ spec/support/controllers/nyauth/session_concern.rb
@@ -1,6 +1,6 @@
 RSpec.shared_examples 'Nyauth::SessionConcern' do
   describe '#sign_in' do
-    subject { controller.sign_in(user) }
+    subject { controller.__send__(:sign_in, user) }
 
     context 'as user' do
       let(:options) { { as: :user } }
@@ -10,19 +10,19 @@ RSpec.shared_examples 'Nyauth::SessionConcern' do
         it '#signed_in? should change result from false to true 'do
           expect {
             subject
-          }.to change { controller.signed_in?(options) }.from(false).to(true)
+          }.to change { controller.__send__(:signed_in?, options) }.from(false).to(true)
         end
 
         it '#signed_in?(as: :admin) should not change result from false'do
           expect {
             subject
-          }.not_to change { controller.signed_in?(as: :admin) }.from(false)
+          }.not_to change { controller.__send__(:signed_in?, as: :admin) }.from(false)
         end
 
         it '#current_authenticated should change from result nil to user 'do
           expect {
             subject
-          }.to change { controller.current_authenticated }.from(nil).to(user)
+          }.to change { controller.__send__(:current_authenticated) }.from(nil).to(user)
         end
       end
 
@@ -31,13 +31,13 @@ RSpec.shared_examples 'Nyauth::SessionConcern' do
         it '#signed_in? should not change result from false'do
           expect {
             subject
-          }.not_to change { controller.signed_in?(options) }.from(false)
+          }.not_to change { controller.__send__(:signed_in?, options) }.from(false)
         end
 
         it '#current_authenticated should not change result from nil'do
           expect {
             subject
-          }.not_to change { controller.current_authenticated }.from(nil)
+          }.not_to change { controller.__send__(:current_authenticated) }.from(nil)
         end
       end
     end
