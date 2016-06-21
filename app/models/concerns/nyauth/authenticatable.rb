178835d8c8b6734f9fa629455d6232252145395b
ppworks <koshikawa@ppworks.jp>
2016-06-21 15:59:49 +0900

    Separate method

--- app/models/concerns/nyauth/authenticatable.rb
+++ app/models/concerns/nyauth/authenticatable.rb
@@ -13,10 +13,14 @@ module Nyauth
 
     module ClassMethods
       def authenticate(given_email, given_password)
-        record = with_given_email(given_email).last
+        record = candidate_for_authentication(given_email)
         return nil unless record
 
         record.verify_password?(given_password) ? record : nil
+      end
+
+      def candidate_for_authentication(given_email)
+        with_given_email(given_email).last
       end
     end
   end
